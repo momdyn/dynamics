db6e0bfa241a0fd019c79c7320cf92f2202fc8c5
Eliott <radcli14@gmail.com>
2022-08-02 08:48:39 +0200

    Add the dtex function

--- dynamicsutilities.py
+++ dynamicsutilities.py
@@ -1,2 +1,29 @@
+"""
+Uses `IPython.display` to show formatted equation with an equals sign between terms given `str` or `Symbol`-like arguments.
+
+Example
+-------
+>> # String-formatted argument
+>> arg1 = r'\boldsymbol{r}'
+>>
+>> # Sympy-formatted argument
+>> N = ReferenceFrame('N')
+>> x, y = symbols('x, y')
+>> arg2 = x*N.x + Y*N.y
+>>
+>> # Formatted equation
+>> dtex(arg1, arg2)
+"""
 def dtex(*args):
-  return
+    tex = ''
+    for arg in args:
+        if isinstance(arg, str):
+            # Handle string arguments
+            tex += f'${arg}$ $\, = \,$ '
+        else:
+            # Handle SymPy arguments
+            tex += f'${mlatex(arg)}$ $\, = \,$ '
+    
+    # Trim off the last equals sign
+    tex = tex[:-10]  
+    display(Latex(tex))
